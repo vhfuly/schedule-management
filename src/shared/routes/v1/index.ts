694d27826e6a4d109d7645fc933c881e19f48d01
vhfuly <victor_hugo_fuly@hotmail.com>
2022-02-13 09:58:55 -0300

    feat: add new routes

--- src/shared/routes/v1/index.ts
+++ src/shared/routes/v1/index.ts
@@ -2,5 +2,7 @@ import { Router } from 'express';
 import { schedulesRoutes } from './schedules.routes';
 
 const routes = Router();
+
 routes.use('/schedules', schedulesRoutes)
+
 export { routes as routesApi }
--- src/shared/routes/v1/schedules.routes.ts
+++ src/shared/routes/v1/schedules.routes.ts
@@ -1,11 +1,17 @@
-import { ScheduleController } from '../../../Schedules/controllers/ScheduleController';
+import { ScheduleRuleController } from '../../../Schedules/controllers/ScheduleRuleController';
 import { Router } from 'express';
+import { scheduleValidation } from '../../validations/ScheduleValidation';
+import { ScheduleAvailableController } from '../../../Schedules/controllers/ScheduleAvailableController';
 
-const scheduleController = new ScheduleController();
-
+const scheduleRuleController = new ScheduleRuleController();
+const scheduleAvailableController  = new ScheduleAvailableController()
 const routes = Router();
 
-routes.get('/', scheduleController.index);
-routes.post('/', scheduleController.store);
+routes.get('/available', scheduleValidation.date, scheduleAvailableController.index);
+
+routes.get('/', scheduleRuleController.index);
+routes.post('/', scheduleValidation.scheduleType, scheduleRuleController.create);
+routes.delete('/:id', scheduleRuleController.delete);
+
 
 export { routes as schedulesRoutes }
